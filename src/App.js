27f24e22516b84f2d6f562b07bd0b6b60176d14b
Harrison <harrisonxm@gmail.com>
2017-10-06 11:05:41 -0700

    added more methods

--- src/App.js
+++ src/App.js
@@ -12,29 +12,35 @@ class App extends Component {
     }
     this.addNote = this.addNote.bind(this);
     this.handleUpdate = this.handleUpdate.bind(this);
+    this.updateTasks = this.updateTasks.bind(this);
   }
   addNote(e){
-    e.preventDefault()
+    //prevent page from refreshing after submission
+    e.preventDefault();
     //store each task as tuple to allow changing left or right of columns
-    let task = [[e.target.name, this.state.input]]
+    let task = [[e.target.name, this.state.input]];
     //before adding into specific key in local storage, grab previously stored 'tasks'
     let storedTasks = JSON.parse(localStorage.getItem('tasks')) || [];
     //first parameter is key name in local storage, destructure arrays and combine then stringify
     localStorage.setItem('tasks', JSON.stringify([...storedTasks, ...task]) );
-    this.setState({tasks: localStorage.tasks})
+    this.setState({tasks: localStorage.tasks});
     //grab specific input field
     let target = e.target.getElementsByClassName('input');
     //after submit note clear input line
     target[0].value = '';
   }
   handleUpdate(e){
-    this.setState({input : e.target.value})
+    this.setState({input : e.target.value});
+  }
+  updateTasks(arr){
+    //need to pass a callback func to update App.js without refreshing
+    this.setState({tasks: arr});
   }
   render() {
     let columns = this.state.status.map((s, index) => {
       return(
         <div className="column" key={index}>
-          <Column status={s} tasks={this.state.tasks} />
+          <Column status={s} tasks={this.state.tasks} updateTasks={this.updateTasks} />
           <form className="addnote" name={s} onSubmit={this.addNote}>
             <input type="text" className="input" placeholder="add a note" onChange={this.handleUpdate} required></input>
             <input type="submit" value="Add"></input>
--- src/column.js
+++ src/column.js
@@ -6,30 +6,125 @@ class Column extends Component {
     this.state = {
       tasks: this.props.tasks || []
     }
+    this.moveLeft = this.moveLeft.bind(this);
+    this.moveRight = this.moveRight.bind(this);
+    this.renderTasks = this.renderTasks.bind(this);
   }
   componentWillReceiveProps(newProps){
     if(localStorage.tasks){
       let updatedTasks = JSON.parse(localStorage.getItem('tasks'));
+      //check if localStorage has updated if so then setState to rerender
       if(updatedTasks !== this.state.tasks){
         this.setState({tasks: updatedTasks})
       }
     }
   }
-  render(){
-    console.log(this.state.tasks)
-    let tasks = this.state.tasks.map((task, index) => {
+  renderTasks(){
+    //return list of specific lists
+    return this.state.tasks.map((task, index) => {
+      //render tasks on correct columns, if status doesnt match props status then don't render 
       let t = task[0] === this.props.status ? task[1] : null;
       if(t){
-        return (
-          <li key={index}>{t}</li>
-        )
+        if(task[0] === 'New'){
+          return(
+            <li key={index}>
+            {t}
+            <button className="btn btn-xs btn-success" data-index={index} data-task={task[0]} onClick={this.moveRight}>Right</button>
+            </li>
+          )
+        }
+        if(task[0] === 'In Process'){
+          return(
+            <li key={index}>
+              {t}
+              <button className="btn btn-xs btn-success" data-index={index} data-task={task[0]} onClick={this.moveRight}>Right</button>
+              <button className="btn btn-xs btn-danger" data-index={index} data-task={task[0]} onClick={this.moveLeft}>Left</button>
+            </li>
+          )
+        }
+        if(task[0] === 'Review'){
+          return(
+            <li key={index}>
+              {t}
+              <button className="btn btn-xs btn-success" data-index={index} data-task={task[0]} onClick={this.moveRight}>Right</button>
+              <button className="btn btn-xs btn-danger" data-index={index} data-task={task[0]} onClick={this.moveLeft}>Left</button>
+            </li>
+          )
+        }
+        if(task[0] === 'Completed'){
+          return(
+            <li key={index}>
+              {t}
+              <button className="btn btn-xs btn-danger" data-index={index} data-task={task[0]} onClick={this.moveLeft}>Left</button>
+            </li>
+          )
+        }
       }
     })
+  }
+  moveLeft(e){
+    //grab specific task and index
+    let t = e.target.dataset.task;
+    let i = e.target.dataset.index;
+    let arr = [...this.state.tasks];
+    switch(t){
+      //depending on status of task if left button pressed swap status and update and local Storage
+      case 'In Process':
+        arr[i][0] = "New";
+        localStorage.tasks = JSON.stringify(arr);
+        this.setState({tasks: arr});
+        this.props.updateTasks(arr);
+        break;
+      case 'Review':
+        arr[i][0] = "In Process";
+        localStorage.tasks = JSON.stringify(arr);
+        this.setState({tasks: arr});
+        this.props.updateTasks(arr);
+        break;
+      case 'Completed':
+        arr[i][0] = "Review";
+        localStorage.tasks = JSON.stringify(arr);
+        this.setState({tasks: arr});
+        this.props.updateTasks(arr);
+        break;
+      default:
+        break;
+      }
+  }
+  moveRight(e){
+    let t = e.target.dataset.task;
+    let i = e.target.dataset.index;
+    let arr = [...this.state.tasks];
+    switch(t){
+      //depending on status of task if right button pressed swap status and update state and localStorage
+      case 'New':
+        arr[i][0] = "In Process";
+        localStorage.tasks = JSON.stringify(arr);
+        this.setState({tasks: arr});
+        this.props.updateTasks(arr);
+        break;
+      case 'In Process':
+        arr[i][0] = "Review";
+        localStorage.tasks = JSON.stringify(arr);
+        this.setState({tasks: arr});
+        this.props.updateTasks(arr);
+        break;
+      case 'Review':
+        arr[i][0] = "Completed";
+        localStorage.tasks = JSON.stringify(arr);
+        this.setState({tasks: arr});
+        this.props.updateTasks(arr);
+        break;
+      default:
+        break;
+      }
+  }
+  render(){
     return(
-      <div>
+      <div className="col">
        <h1>{this.props.status}</h1>
-       <ul>
-        {tasks}
+       <ul className="tasklist">
+        {this.renderTasks()}
        </ul>
       </div>
     )
